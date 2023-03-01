59ff1a1db7fcef4cf4c7b9118e0b304ac38b9a16
Francesco Saverio Zuppichini <francesco.zuppichini@gmail.com>
2023-03-01 10:27:57 +0100

    Update ubuntu20_04_setup.sh
    addded `xclip`

--- ubuntu20_04_setup.sh
+++ ubuntu20_04_setup.sh
@@ -60,6 +60,7 @@ curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo 
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
 sudo apt update
+sudo apt install xclip
 sudo apt install albert -y
 ## Plank
 sudo apt install plank -y
